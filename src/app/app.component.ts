c96fb33e25f441e5b5288bb91c641d21fbc70fbe
Emilly Wolski <emilly.wolski@gmail.com>
2024-03-16 12:20:30 -0300

    Criação da estrutura dos components de LOGIN e AUTOCADASTRO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutocadastroComponent } from "./components/autocadastro/autocadastro.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AutocadastroComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'lol-lavanderia-online';
