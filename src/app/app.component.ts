588df1b3d16b6361d8cb53db6e31471efeaf02f7
Emilly Wolski <emilly.wolski@gmail.com>
2024-03-18 21:28:39 -0300

    Criação inicial do modal de aprovar ou recusar orçamento.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AutocadastroComponent } from "./components/autocadastro/autocadastro.component";
 import { LoginComponent } from "./components/login/login.component";
+import { InicialClienteComponent } from "./components/inicial-cliente/inicial-cliente.component";
+import { ModalOrcamentoComponent } from "./components/modal-orcamento/modal-orcamento.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, AutocadastroComponent, LoginComponent]
+    imports: [RouterOutlet, AutocadastroComponent, LoginComponent, InicialClienteComponent, ModalOrcamentoComponent]
 })
 export class AppComponent {
   title = 'lol-lavanderia-online';
