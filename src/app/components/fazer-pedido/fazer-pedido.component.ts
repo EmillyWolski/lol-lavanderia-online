3edf7db433a3d1b9956de9b4b37f83e447d3bc12
Julio Yamaguchi <julioyamaguchi@ufpr.br>
2024-03-21 17:37:40 -0300

    Criação dos arquivos + Html do componente fazer-pedido

---
+++ src/app/components/fazer-pedido/fazer-pedido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fazer-pedido',
+  standalone: true,
+  imports: [],
+  templateUrl: './fazer-pedido.component.html',
+  styleUrl: './fazer-pedido.component.css'
+})
+export class FazerPedidoComponent {
+
+}
