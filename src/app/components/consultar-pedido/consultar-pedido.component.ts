c93f8f0e1301370ac02a1e08d5d94f6e11ac3a21
Emilly Wolski <emilly.wolski@gmail.com>
2024-03-24 14:06:56 -0300

    Ajustes nos modais e criação do component consultar-pedido

---
+++ src/app/components/consultar-pedido/consultar-pedido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-pedido',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultar-pedido.component.html',
+  styleUrl: './consultar-pedido.component.css'
+})
+export class ConsultarPedidoComponent {
+
+}
