1073eb457f1112084a3e4c081faa8ca8b909b145
Emilly Wolski <emilly.wolski@gmail.com>
2024-03-24 17:56:09 -0300

    Criação do HTML e CSS de Consultar Pedido e criação da pasta Service

--- src/app/components/consultar-pedido/consultar-pedido.component.ts
+++ src/app/components/consultar-pedido/consultar-pedido.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConsultarPedidoService } from '../../services/consultar-pedido';
 
 @Component({
   selector: 'app-consultar-pedido',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './consultar-pedido.component.html',
   styleUrl: './consultar-pedido.component.css'
 })
+
 export class ConsultarPedidoComponent {
 
+  codigoPedido!: number; // Alterado para número para corresponder ao tipo de ID
+  pedidoEncontrado: any;
+
+  constructor(private consultarPedidoService: ConsultarPedidoService) { }
+
+  // Método para buscar o pedido quando o usuário submeter o formulário
+  onSubmit() {
+    // Chamar o serviço para buscar o pedido com o ID fornecido
+    this.pedidoEncontrado = this.consultarPedidoService.buscarPedidoPorId(this.codigoPedido);
+    if (!this.pedidoEncontrado) {
+      console.log('Pedido não encontrado');
+      // Lógica para lidar com pedido não encontrado, se necessário
+    }
+  }
 }
+
