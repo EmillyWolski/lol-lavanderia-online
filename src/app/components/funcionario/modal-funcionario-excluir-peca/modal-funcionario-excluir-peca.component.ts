b11fb645cced47f2c320c70b5faacc8890271b6b
Emanuelly Wolski <wolski.reserva@gmail.com>
2024-03-30 13:23:29 -0300

    Criação do modal de excluir peça

---
+++ src/app/components/funcionario/modal-funcionario-excluir-peca/modal-funcionario-excluir-peca.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-funcionario-excluir-peca',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-funcionario-excluir-peca.component.html',
+  styleUrl: './modal-funcionario-excluir-peca.component.css'
+})
+export class ModalFuncionarioExcluirPecaComponent {
+
+}
