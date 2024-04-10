a7f5a005e41ddd1eef96fe2a2b2655d451d92bf4
Emanuelly Wolski <wolski.reserva@gmail.com>
2024-04-09 21:07:07 -0300

    Tentativa de implementação da funcionalidade de Autocadastro

--- src/app/components/autocadastro/autocadastro.component.ts
+++ src/app/components/autocadastro/autocadastro.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-autocadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './autocadastro.component.html',
-  styleUrl: './autocadastro.component.css'
+  styleUrls: ['./autocadastro.component.css']
 })
 export class AutocadastroComponent {
+  autocadastroForm = this.fb.group({
+    cpf: ['', Validators.required],
+    nome: ['', Validators.required],
+    emailCadastro: ['', [Validators.required, Validators.email]],
+    endereco: ['', Validators.required],
+    telefone: ['', Validators.required]
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    const { cpf, nome, emailCadastro, endereco, telefone } = this.autocadastroForm.value;
+    if (this.autocadastroForm.valid) {
+      alert(`Dados do formulário:\n\nCPF: ${cpf}\nNome: ${nome}\nE-mail: ${emailCadastro}\nEndereço: ${endereco}\nTelefone: ${telefone}`);
+      this.autocadastroForm.reset();
+    } else {
+      this.autocadastroForm.markAllAsTouched();
+    }
+  }  
 }
+
+
+
+
+
+
