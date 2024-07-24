5a987cde9430ccd909926b0298097ae01e627843
elito120 <elito120@gmail.com>
2024-07-24 20:40:53 -0300

    Inicio mudanças no login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideEnvironmentNgxMask, provideNgxMask } from 'ngx-mask';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideNgxMask(),
-    provideEnvironmentNgxMask()
+    provideEnvironmentNgxMask(),
+    provideHttpClient()
   ]
 };
--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,34 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Pessoa } from '../../shared/models/pessoa.model';
 import { AutocadastroService } from '../../services/autocadastro/autocadastro.service';
 import { Router } from '@angular/router';
 
-
 @Injectable({
   providedIn: 'root'
 })
-
 export class LoginService {
 
   private pessoaLogada: Pessoa | null = null; // Para armazenar a pessoa logada
 
-  constructor(private autocadastroService: AutocadastroService, private router: Router) {}
+  
+  constructor(private http: HttpClient, private autocadastroService: AutocadastroService, private router: Router) {}
 
   login(email: string, senha: string): Observable<Pessoa | null> {
-    // Obtém a lista de todas as pessoas cadastradas
-    const pessoas = this.autocadastroService.listarTodos();
-  
-    // Verifica se há uma pessoa com o email e senha fornecidos
-    const pessoa = pessoas.find(p => p.email && p.email.toLowerCase() === email.toLowerCase() && p.senha === senha);
-    
-    // Armazena a pessoa logada
-    this.pessoaLogada = pessoa || null;
-    if (this.pessoaLogada) {
-      localStorage.setItem('pessoaLogadaId', this.pessoaLogada.id.toString());
-    }
-    return of(this.pessoaLogada);
-    
+    const url = "http://localhost:3000/usuarios"; // URL do endpoint de login
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { email, senha };
+
+    return this.http.post<Pessoa>(url, body, { headers }).pipe(
+      map(pessoa => {
+        this.pessoaLogada = pessoa || null;
+        if (this.pessoaLogada) {
+          localStorage.setItem('pessoaLogadaId', this.pessoaLogada.id.toString());
+        }
+        return this.pessoaLogada;
+      }),
+      catchError(error => {
+        console.error('Erro no login:', error);
+        return of(null);
+      })
+    );
   }  
 
   getPessoaLogada(): Pessoa | null {
